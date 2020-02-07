ec1814e5fdc89cacef70dd73ec1d658bec19fc1b
LyangHiga <lyang_higa@poli.ufrj.br>
2020-02-07 14:53:11 -0300

    Add Delete button to Mini Palette

--- src/styles/MiniPalettesStyles.js
+++ src/styles/MiniPalettesStyles.js
@@ -5,10 +5,10 @@ export default {
         padding: '0.5rem',
         position: 'relative',
         overflow: 'hidden',
-        '&:hover':{
-            cursor:'pointer'
-        },
-        border: '1px solid black'
+        cursor:'pointer',
+        '&:hover svg':{
+            opacity: 1
+        }
     },
     colors:{
         backgroundColor: '#dae1e4',
@@ -38,5 +38,18 @@ export default {
         margin: '0 auto',
         position: 'relative',
         marginBottom: '-3.5px'
+    },
+    delete: {},
+    deleteIcon:{
+        color: 'white',
+        backgroundColor: '#eb3d30',
+        width: '20px',
+        height: '20px',
+        position: 'absolute',
+        right: '0px',
+        top: '0px',
+        padding: '10px',
+        zIndex: 10,
+        opacity: 0,
     }
 };
