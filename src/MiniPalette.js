68c336dbf087ef9f262bb11596b112619bc549d9
LyangHiga <lyang_higa@poli.ufrj.br>
2020-02-11 20:02:42 -0300

    Refactor Mini Palette with Pure Component

--- src/MiniPalette.js
+++ src/MiniPalette.js
@@ -1,13 +1,14 @@
-import React, {Component} from 'react';
+import React, {PureComponent} from 'react';
 import { withStyles } from '@material-ui/core/styles';
 import DeleteIcon from '@material-ui/icons/Delete';
 import styles from './styles/MiniPalettesStyles';
 
-class MiniPalette extends Component{
+class MiniPalette extends PureComponent{
     
     constructor(props){
         super(props);
         this.removePalette = this.removePalette.bind(this);
+        this.handleClick = this.handleClick.bind(this);
     };
 
     removePalette(evt){
@@ -15,8 +16,13 @@ class MiniPalette extends Component{
         this.props.openDialog(this.props.id);
     };
 
+    handleClick(){
+        this.props.goToPalette(this.props.id);
+    }
+
     render(){
-        const {classes, paletteName, emoji, colors, handleClick} = this.props;
+        const {classes, paletteName, emoji, colors} = this.props;
+        console.log("RENDERING: ", paletteName);
         const miniColorBoxes = colors.map(color =>(
             <div 
                 className={classes.miniColor} 
@@ -25,7 +31,7 @@ class MiniPalette extends Component{
             />
         ));
         return(
-            <div className={classes.root} onClick={handleClick}>
+            <div className={classes.root} onClick={this.handleClick}>
                 <DeleteIcon 
                     className={classes.deleteIcon} 
                     style={{transition: 'all 0.3s ease-in-out'}}
--- src/PaletteList.js
+++ src/PaletteList.js
@@ -25,6 +25,7 @@ class PaletteList extends Component{
         this.openDialog = this.openDialog.bind(this);
         this.closeDialog = this.closeDialog.bind(this);
         this.handleDelete = this.handleDelete.bind(this);
+        this.goToPalette = this.goToPalette.bind(this);
     };
 
     openDialog(id){
@@ -59,7 +60,7 @@ class PaletteList extends Component{
                             <CSSTransition key={p.id} classNames='fade' timeout={500}>
                                 <MiniPalette 
                                     {...p} 
-                                    handleClick={() => this.goToPalette(p.id)}
+                                    goToPalette={this.goToPalette}
                                     openDialog={this.openDialog}    
                                     key={p.id}
                                     id={p.id}
