7d07a5e31fbf6d4beace61a1a0fd6a70240ce3e1
LyangHiga <lyang_higa@poli.ufrj.br>
2020-02-28 14:34:23 -0300

    Updates

---
+++ precache-manifest.580cfec66a9e5fcdb58e3f8d8ef20d09.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "917afbe9b376bbe2fc3dbf140091abbf",
+    "url": "/react-colors/index.html"
+  },
+  {
+    "revision": "3580dfbf20ed75f702bc",
+    "url": "/react-colors/static/css/2.753ea2eb.chunk.css"
+  },
+  {
+    "revision": "105d2c18f75a757b9be6",
+    "url": "/react-colors/static/css/main.3df82b2f.chunk.css"
+  },
+  {
+    "revision": "3580dfbf20ed75f702bc",
+    "url": "/react-colors/static/js/2.e448779f.chunk.js"
+  },
+  {
+    "revision": "42c3c94a3e26be691c31b5d2b0c63158",
+    "url": "/react-colors/static/js/2.e448779f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "105d2c18f75a757b9be6",
+    "url": "/react-colors/static/js/main.d7921966.chunk.js"
+  },
+  {
+    "revision": "ae8ea1fcad6662b0b11c",
+    "url": "/react-colors/static/js/runtime-main.63af33ed.js"
+  },
+  {
+    "revision": "8268f64ec93d309ced99ae213ef44169",
+    "url": "/react-colors/static/media/bg.8268f64e.svg"
+  }
+]);
