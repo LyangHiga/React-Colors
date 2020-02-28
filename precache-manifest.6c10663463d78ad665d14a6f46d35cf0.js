c7dcc97a2efdd598d1cddc91dca6223026c02837
LyangHiga <lyang_higa@poli.ufrj.br>
2020-02-28 14:41:37 -0300

    Updates

---
+++ precache-manifest.6c10663463d78ad665d14a6f46d35cf0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c71eb24b98e516129b144313ca0c4af4",
+    "url": "/React-Colors/index.html"
+  },
+  {
+    "revision": "3580dfbf20ed75f702bc",
+    "url": "/React-Colors/static/css/2.753ea2eb.chunk.css"
+  },
+  {
+    "revision": "5526644e17ceff75bc57",
+    "url": "/React-Colors/static/css/main.3df82b2f.chunk.css"
+  },
+  {
+    "revision": "3580dfbf20ed75f702bc",
+    "url": "/React-Colors/static/js/2.e448779f.chunk.js"
+  },
+  {
+    "revision": "42c3c94a3e26be691c31b5d2b0c63158",
+    "url": "/React-Colors/static/js/2.e448779f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5526644e17ceff75bc57",
+    "url": "/React-Colors/static/js/main.6b8b099d.chunk.js"
+  },
+  {
+    "revision": "a54f4576195ead76d1f4",
+    "url": "/React-Colors/static/js/runtime-main.c058fc3a.js"
+  },
+  {
+    "revision": "8268f64ec93d309ced99ae213ef44169",
+    "url": "/React-Colors/static/media/bg.8268f64e.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-colors/precache-manifest.580cfec66a9e5fcdb58e3f8d8ef20d09.js"
+  "/React-Colors/precache-manifest.6c10663463d78ad665d14a6f46d35cf0.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-colors/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/React-Colors/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
