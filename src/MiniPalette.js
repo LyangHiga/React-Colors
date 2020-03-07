42ea30de4c892bcfe5b0f194aeaaaa8647cda196
LyangHiga <lyang_higa@poli.ufrj.br>
2020-03-06 22:42:34 -0300

    Mini Palette with hooks

--- src/MiniPalette.js
+++ src/MiniPalette.js
@@ -1,50 +1,39 @@
-import React, {PureComponent} from 'react';
+import React, { memo} from 'react';
 import { withStyles } from '@material-ui/core/styles';
 import DeleteIcon from '@material-ui/icons/Delete';
 import styles from './styles/MiniPalettesStyles';
 
-class MiniPalette extends PureComponent{
-    
-    constructor(props){
-        super(props);
-        this.removePalette = this.removePalette.bind(this);
-        this.handleClick = this.handleClick.bind(this);
-    };
-
-    removePalette(evt){
-        evt.stopPropagation();
-        this.props.openDialog(this.props.id);
-    };
-
-    handleClick(){
-        this.props.goToPalette(this.props.id);
+function MiniPalette(props){
+    const removePalette = (e) => {
+        e.stopPropagation();
+        props.openDialog();
     }
+    const handleClick = () => props.goToPalette(this.props.id);
+    const {classes, paletteName, emoji, colors} = props;
 
-    render(){
-        const {classes, paletteName, emoji, colors} = this.props;
-        const miniColorBoxes = colors.map(color =>(
-            <div 
-                className={classes.miniColor} 
-                style={{backgroundColor: color.color}}
-                key={color.name}
+    const miniColorBoxes = colors.map(color =>(
+        <div 
+            className={classes.miniColor} 
+            style={{backgroundColor: color.color}}
+            key={color.name}
+        />
+    ));
+
+    return(
+        <div className={classes.root} onClick={handleClick}>
+            <DeleteIcon 
+                className={classes.deleteIcon} 
+                style={{transition: 'all 0.3s ease-in-out'}}
+                onClick={removePalette}
             />
-        ));
-        return(
-            <div className={classes.root} onClick={this.handleClick}>
-                <DeleteIcon 
-                    className={classes.deleteIcon} 
-                    style={{transition: 'all 0.3s ease-in-out'}}
-                    onClick={this.removePalette}
-                />
-                <div className={classes.colors}>
-                    {miniColorBoxes}
-                </div>
-                <h5 className={classes.title}>
-                    {paletteName} <spam className={classes.emoji}>{emoji}</spam>
-                </h5>          
+            <div className={classes.colors}>
+                {miniColorBoxes}
             </div>
-        );
-    };
+            <h5 className={classes.title}>
+                {paletteName} <spam className={classes.emoji}>{emoji}</spam>
+            </h5>          
+        </div>
+    );
 }
 
-export default withStyles(styles)(MiniPalette);
+export default memo(withStyles(styles)(MiniPalette));
