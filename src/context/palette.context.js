7fae617fd99a70649312e794216381b0acfa14f7
LyangHiga <lyang_higa@poli.ufrj.br>
2020-03-15 19:48:28 -0300

    Using Local Storage

--- src/context/palette.context.js
+++ src/context/palette.context.js
@@ -1,18 +1,17 @@
-import React, { createContext, useReducer } from 'react';
+import React, { createContext } from 'react';
 import seedColors from '../seedColors';
 import paletteReducer from '../reducers/palette.reducer';
+import { useLocalStorageReducer } from '../hooks/useLocalStorageReducer';
 
 export const PalettesContext = createContext();
 export const DispatchContext = createContext();
-// TODO create Local Storage reducer
-// checkout, merge, deploy
+
 export function PalettesProvider(props) {
-  const [palettes, dispatch] = useReducer(paletteReducer, seedColors);
-  //   const findPalette = id => {
-  //     return palettes.find(function(palette) {
-  //       return palette.id === id;
-  //     });
-  //   };
+  const [palettes, dispatch] = useLocalStorageReducer(
+    'palettes',
+    seedColors,
+    paletteReducer
+  );
   const findPalette = id => {
     const found = palettes.filter(palette =>
       palette.id === id ? palette : null
